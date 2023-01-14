38ed27f02e90ff98736e8328e5aff9afac157ad1
Nate J.M <nate@natejonah.com>
2023-01-14 22:19:45 +0000

    Create MainContent component

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+export default function MainContent() {
+  return (
+    <main className="main-content">
+      <h1 className="title">Fun facts about React</h1>
+      <ul className="facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally </li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  )
+}
