dd6ecd28f1c6416c33983443dc1786b49d123b46
Nate J.M <nate@natejonah.com>
2023-01-22 11:47:01 +0000

    Complete next challenge - style navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
+import logo from '../react-logo-sm.png';
+
 export default function Navbar() {
     return (
-        <nav>
-            <h1>Navbar component here</h1>
+        <nav className='navbar'>
+            <img className='logo' src={logo} alt="React Logo" />
+            <h3 className='site-title'>ReactFacts</h3>
+
+            <h4 className='site-subtitle'>React Course - Project 1</h4>
         </nav>
     )
 }
