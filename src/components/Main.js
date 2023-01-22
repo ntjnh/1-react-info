68cb33bc371e5db13c1871a813d33ed01e04a9df
Nate J.M <nate@natejonah.com>
2023-01-22 11:25:44 +0000

    Complete first challenge

---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+export default function Main() {
+    return (
+        <main>
+            <h1>Main component is here</h1>
+        </main>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>Navbar component here</h1>
+        </nav>
+    )
+}
