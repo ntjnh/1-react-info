4e3d384a851c2c65d8de259689f686ecdf05cc8b
Nate J.M <nate@natejonah.com>
2023-01-22 11:20:48 +0000

    Start afresh to begin properly building the page

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <small>&copy; 2023 Nate Dev. All rights reserved.</small>
-    </footer>
-  )
-}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MainContent from './components/MainContent';
 import './index.css';
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  )
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render();
