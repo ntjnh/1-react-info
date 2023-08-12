e71edabd6752897bf65ee7b1c278a99beb5a9949
Nate J.M <nate@natejonah.com>
2023-08-12 15:42:32 +0100

    Add event handler to toggle dark mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 
 export default function App() {
+    const [isDark, setIsDark] = useState(false)
+
+    const handleToggle = () => setIsDark(prev => !prev)
+
     return (
-        <div>
-            <Navbar />
-            <Main />
+        <div className='container'>
+            <Navbar toggleDarkMode={handleToggle} darkMode={isDark} />
+            <Main toggleDarkMode={handleToggle} darkMode={isDark} />
         </div>
     )
 }
